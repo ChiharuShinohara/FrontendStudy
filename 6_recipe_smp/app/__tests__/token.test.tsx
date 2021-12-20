5338166fe1f1833c21cbbe89b4fa8265611f62f1
Chiharu Shinohara <chiharu.shinohara@nadia-corp.co.jp>
2021-12-20 13:52:16 +0900

    testの削除

--- 6_recipe_smp/app/__tests__/token.test.tsx
+++
@@ -1,9 +0,0 @@
-import tokenAPI from '../pages/api/token'
-import { AxiosClient } from '../modules/request';
-
-test('tokenAPI', async()=>{
-    const axios = AxiosClient();
-    await expect(axios.post('token', {params: "rr"})).rejects.toMatch("err");
-})
-
-
